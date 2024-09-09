67e89047cce09d59f9d6f3692b664118fa2c7ddb
IanD1013 <ian.tung1013@gmail.com>
2024-09-09 12:25:50 +1200

    Add edit user functionality

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,9 +1,10 @@
-import { format } from 'date-fns';
 import useCurrentUser from '@/hooks/useCurrentUser';
+import useEditModal from '@/hooks/useEditModal';
 import useUser from '@/hooks/useUser';
-import React, { useMemo } from 'react';
-import Button from '../Button';
+import { format } from 'date-fns';
+import { useMemo } from 'react';
 import { BiCalendar } from 'react-icons/bi';
+import Button from '../Button';
 
 interface UserBioProps {
   userId: string;
@@ -12,6 +13,8 @@ interface UserBioProps {
 const UserBio = ({ userId }: UserBioProps) => {
   const { data: currentUser } = useCurrentUser();
   const { data: fetchedUser } = useUser(userId);
+
+  const editModal = useEditModal();
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) return null;
@@ -22,7 +25,7 @@ const UserBio = ({ userId }: UserBioProps) => {
   return (
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
-        {currentUser?.id === userId ? <Button secondary label="Edit" onClick={() => {}} /> : <Button secondary label="Follow" onClick={() => {}} />}
+        {currentUser?.id === userId ? <Button secondary label="Edit" onClick={editModal.onOpen} /> : <Button secondary label="Follow" onClick={() => {}} />}
       </div>
 
       <div className="mt-8 px-4">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from 'next-auth/react';
 
 import LoginModal from '@/components/modals/LoginModal';
 import RegisterModal from '@/components/modals/RegisterModal';
+import EditModal from '@/components/modals/EditModal';
 import '@/styles/globals.css';
 import Layout from '../components/Layout';
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout>
