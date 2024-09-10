721de799a2c986acdbe9a319296ec29ffab6deaa
IanD1013 <ian.tung1013@gmail.com>
2024-09-10 14:08:24 +1200

    Add image upload functionality

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -6,6 +6,7 @@ import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import Modal from '../Modal';
 import Input from '../Input';
+import ImageUpload from '../ImageUpload';
 
 const EditModal = () => {
   const { data: currentUser } = useCurrentUser();
@@ -46,6 +47,8 @@ const EditModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
+      <ImageUpload value={profileImage} disabled={isLoading} onChange={(image) => setProfileImage(image)} label="Upload profile image" />
+      <ImageUpload value={coverImage} disabled={isLoading} onChange={(image) => setCoverImage(image)} label="Upload cover image" />
       <Input placeholder="Name" onChange={(e) => setName(e.target.value)} value={name} disabled={isLoading} />
       <Input placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username} disabled={isLoading} />
       <Input placeholder="Bio" onChange={(e) => setBio(e.target.value)} value={bio} disabled={isLoading} />
