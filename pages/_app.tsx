d773bf24cf0a6d660d9ba4f072a295c78440694d
IanD1013 <ian.tung1013@gmail.com>
2024-09-12 09:58:53 +1200

    Change page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,26 @@
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
-import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
+import EditModal from '@/components/modals/EditModal';
 import LoginModal from '@/components/modals/LoginModal';
 import RegisterModal from '@/components/modals/RegisterModal';
-import EditModal from '@/components/modals/EditModal';
 import '@/styles/globals.css';
 import Layout from '../components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>Chirper</title>
+      </Head>
       <Toaster />
       <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </Layout>
     </SessionProvider>
   );
